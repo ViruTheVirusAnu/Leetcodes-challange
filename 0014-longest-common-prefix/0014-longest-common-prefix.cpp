86d37dc091daa6e346e228f1f0fc1af402460195
Viru <150415485+ViruTheVirusAnu@users.noreply.github.com>
2026-09-06 18:57:58 +0530

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 12 MB (24.79%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& v) {
-        string ans="";
-        sort(v.begin(),v.end());
-        int n=v.size();
-        string first=v[0],last=v[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
-            }
-            ans+=first[i];
-        }
-        return ans;
+    string longestCommonPrefix(vector<string>& strs) {
+    
+    // when input case is emptyy
+    if(strs.empty()) return "";
+
+    // sort the string lexicographically
+    sort(strs.begin(),strs.end());
+
+    // first string in sorted order
+    string first = strs[0];
+
+    // last string in sorted order
+    string last = strs[strs.size()-1];
+
+    // to store common prefix
+    string ans ="";
+
+    // compare which shorter string
+    int minLength = min(first.size(),last.size());
+
+    // compare charachters of first and last string
+    for(int i = 0; i < minLength;i++)
+    {
+        if(first[i] != last[i]) break;
+        ans += first[i];
+    }
+    return ans;
+
+
     }
 };
