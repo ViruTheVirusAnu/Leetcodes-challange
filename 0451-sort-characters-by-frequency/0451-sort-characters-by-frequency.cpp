3dc6c29a0b39d93cc68c65be5ee0df51e86a171d
Viru <150415485+ViruTheVirusAnu@users.noreply.github.com>
2026-09-19 22:31:44 +0530

    0451-SORT-CHARACTERS-BY-FREQUENCY Stats: Time: 4 ms (37.6%), Space: 17.5 MB (5.68%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        
-        // Count freq
+
+        // 1. Count frequency
         unordered_map<char, int> freq;
-        for(char ch:s)
-        {
+
+        for(char ch : s) {
             freq[ch]++;
         }
-        
+
+        // 2. Create buckets
+        vector<vector<char>> bucket(s.size() + 1);
+
+        // 3. Put each character in its frequency bucket
+        for(auto it : freq) {
+            char ch = it.first;
+            int count = it.second;
+
+            bucket[count].push_back(ch);
+        }
+
+        // 4. Build answer from highest frequency to lowest
         string ans = "";
 
-        // keep proecessing until map gets empty
-        while(!freq.empty())
-        {
-            char maxchar;
-            int maxFreq = 0;
+        for(int i = s.size(); i >= 1; i--) {
 
-            // find char with max freq
-            for(auto it:freq)
-            {
-                if(it.second > maxFreq){
-                    maxFreq = it.second;
-                    maxchar = it.first;
-                }
+            for(char ch : bucket[i]) {
+                ans += string(i, ch);
             }
-            // Add char in max freq
-            ans += string(maxFreq,maxchar);
+        }
 
-            // remove processed char
-            freq.erase(maxchar);
-        } 
-
-
-return ans;
+        return ans;
     }
 };
